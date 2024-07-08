683c715b7576fdf6f5fdbd624e417a73057b2fd5
kalidas ch <118943313+kalidas31@users.noreply.github.com>
2024-07-08 05:34:12 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,43 @@
+import React from 'react';
+//import Searchautocomplete from './components/search-autocomplete-with-api';
+import TicTacToe from './components/Tic-Tac-Toe';
+//import Githubprofilefinder from './components/github-profile-finder';
+//import notes from './components/keeperpjt/notes';
+//import Header from './components/keeperpjt/Header';
+//import Note from './components/keeperpjt/Note';
+//import Footer from './components/keeperpjt/Footer';
+//import Accordian from './components/accordian';
+//import Randomcolor from './components/randon-color';
+//import Starrating from './components/star-rating';
+//import Imageslider from './components/image-slider';
+//import Loadmoredata from './components/Loadmoredata';
+//import Treeview from './components/Tree-view';
+//import menus from './components/Tree-view/data';
+//import QRcodegenerator from './components/qr-code-generator';
+//import LightDarkMode from './components/light-dark-mode';
+//import Scrollindicator from './scroll-indicator';
+//import Tabtest from './components/custom-tabs/tab-test';
+//import Modeltest from './components/custom-model-popup/model-text';
+
+function App() {
+  return (
+    <div className="App">
+       {/*<Accordian>*/}
+       {/*<Randomcolor/>*/}
+       {/*<Starrating noofstars={10}/>*/}
+       {/*<Imageslider url={'https://picsum.photos/v2/list'} limit={'4'} page={'1'}/>*/}
+       {/*<Loadmoredata/>*/}
+       {/*<Treeview menus={menus}/>*/}
+       {/*<QRcodegenerator/>*/}
+       {/*<LightDarkMode/>*/}
+       {/*<Scrollindicator url={'https://dummyjson.com/products?limit=100'}/>*/}
+       {/*<Tabtest/>*/}
+       {/*<Modeltest/>*/}
+       {/* <Githubprofilefinder/> */}
+       {/* <Searchautocomplete/> */}
+       <TicTacToe/>
+    </div>
+  );
+}
+
+export default App;
